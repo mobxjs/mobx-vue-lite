8cf99ec89e91f23cf10ed01d9b4711e3f75938ed
Robert Soriano <sorianorobertc@gmail.com>
2021-08-13 15:52:33 +0800

    add createGlobalObservable test

---
+++ test/create-global-observable.ts
@@ -0,0 +1,61 @@
+// @ts-ignore
+import { mount  } from '@vue/test-utils'
+import { nextTick } from 'vue'
+import { createGlobalObservable, useLocalObservable } from '../src'
+
+describe('useLocalObservable', () => {
+    const useGlobalObservable = createGlobalObservable(() => {
+        return useLocalObservable(() => ({
+            count: 0,
+            get double() {
+              return this.count * 2
+            },
+            increment() {
+              this.count++
+            }
+          }))
+    })
+
+    it('should work properly in a Vue component', async () => {
+        const Component = {
+            template: `
+                <div data-testid="count">Count: {{ state.count }}</div>
+                <div data-testid="doubled">Doubled: {{ state.double }}</div>
+                <button @click="state.increment">Increment</button>
+            `,
+            setup() {
+                const state = useGlobalObservable()
+                return { state }
+            }
+        }
+
+        const OtherComponent = {
+            template: `
+                <div data-testid="count">Count: {{ state.count }}</div>
+                <div data-testid="doubled">Doubled: {{ state.double }}</div>
+            `,
+            setup() {
+                const state = useGlobalObservable()
+                return { state }
+            }
+        }
+
+        const mainWrapper = mount(Component)
+        const otherWrapper = mount(OtherComponent)
+
+        expect(mainWrapper.find('[data-testid="count"]').text()).toBe('Count: 0')
+        expect(mainWrapper.find('[data-testid="doubled"]').text()).toBe('Doubled: 0')
+        expect(otherWrapper.find('[data-testid="count"]').text()).toBe('Count: 0')
+        expect(otherWrapper.find('[data-testid="doubled"]').text()).toBe('Doubled: 0')
+
+        await mainWrapper.find('button').trigger('click')
+        await mainWrapper.find('button').trigger('click')
+
+        await nextTick()
+
+        expect(mainWrapper.find('[data-testid="count"]').text()).toBe('Count: 2')
+        expect(mainWrapper.find('[data-testid="doubled"]').text()).toBe('Doubled: 4')
+        expect(otherWrapper.find('[data-testid="count"]').text()).toBe('Count: 2')
+        expect(otherWrapper.find('[data-testid="doubled"]').text()).toBe('Doubled: 4')
+    });
+})
