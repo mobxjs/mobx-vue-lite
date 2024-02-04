837cfd18df62477f788e61b7288bdc8e3b66a7d3
wobsoriano <sorianorobertc@gmail.com>
2024-02-03 16:29:31 -0800

    update plugin type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import type { App } from 'vue'
+import type { Plugin as VuePlugin } from 'vue'
 import { createGlobalState as createGlobalObservable } from '@vueuse/core'
 import Observer from './ObserverComponent'
 import { useLocalObservable } from './use-local-observable'
 
-const Plugin = {
-  install: (app: App) => {
+const Plugin: VuePlugin = {
+  install: (app) => {
     app.component('Observer', Observer)
   },
 }
